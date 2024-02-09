1744f042bad07ac2473f90f2113af39cb8365e10
Abdiwawi <abdilahi.jama@mnsu.edu>
2024-02-09 00:13:29 -0600

    Made changes and created a Driver class

--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -1,12 +1,19 @@
 import java.util.Random;
 
 class Philosopher extends Thread {
-  private Chopstick left, right;
+  private Chopstick first, second;
   private Random random;
   private int thinkCount;
+  private int eatCount;
+  private int id;
 
   public Philosopher(Chopstick left, Chopstick right) {
-    this.left = left; this.right = right;
+    if (left.getId() > right.getId()) {
+      this.first = right; this.second = left;
+    }else {
+      this.first = left; this.second = right;
+    }
+
     random = new Random();
   }
 
@@ -14,13 +21,26 @@ class Philosopher extends Thread {
     try {
       while(true) {
         ++thinkCount;
-        if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
+        ++eatCount;
+        ++id;
+        //if (thinkCount % 10 == 0)
+        System.out.println("Philosopher " + id  + " thinks " + thinkCount + " units");
         Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
+        synchronized(first) {
+          // Grab right chopstick
+          System.out.println("Philosopher " + id  + " wants right chopstick");
+          System.out.println("Philosopher " + id  + " has right chopstick");
+          synchronized(second) {
+            // Grab left chopstick
+            System.out.println("Philosopher " + id  + " wants left chopstick");
+            System.out.println("Philosopher " + id + " has left chopstick");
             Thread.sleep(random.nextInt(1000)); // Eat for a while
           }
+
+          System.out.println("Philosopher " + id + " eats for " + eatCount + " units");
+          Thread.sleep(random.nextInt(1000));
+          System.out.println("Philosopher " + id + " releases left chopstick");
+          System.out.println("Philosopher " + id  + " releases right chopstick");
         }
       }
     } catch(InterruptedException e) {}
