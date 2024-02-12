c88df6edc197a5c3f88f73799b67cf1b7925ad6e
Abdiwawi <abdilahi.jama@mnsu.edu>
2024-02-11 22:46:40 -0600

    Finished editing the assignment

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,17 +1,48 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DiningPhilosophers {
 
-  public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
-    
-    for (int i = 0; i < 5; ++i)
-      chopsticks[i] = new Chopstick(i);
-    for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+  public static void main(String[] args) throws InterruptedException, IOException {
+    int np = Integer.parseInt(args[0]);
+    int nc = Integer.parseInt(args[1]);
+    int tt = Integer.parseInt(args[2]);
+    int et = Integer.parseInt(args[3]);
+    int rl = Integer.parseInt(args[4]);
 
+    Philosopher[] philosophers = new Philosopher[np];
+    Chopstick[] chopsticks = new Chopstick[np];
+
+    /**
+     * Check if the file already exists and override
+     */
+    try {
+      File traceFile = new File("Trace.txt");
+      if (traceFile.createNewFile()) {
+        System.out.println("Created a file: " + traceFile.getName());
+      }
+      else {
+        System.out.println("The file already exists");
+        traceFile.delete();
+        System.out.println("Created a new one");
+        traceFile.createNewFile();
+        System.out.println("Done.......");
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    FileWriter traceFileWriter = new FileWriter("Trace.txt");
+
+    for (int i = 0; i < np; ++i)
+      chopsticks[i] = new Chopstick(i);
+    for (int i = 0; i < np; ++i) {
+      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % np], i ,nc ,tt ,et ,rl, traceFileWriter);
       philosophers[i].start();
     }
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < np; ++i)
       philosophers[i].join();
   }
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -1,48 +1,126 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 class Philosopher extends Thread {
-  private Chopstick first, second;
+  private Chopstick right, left;
   private Random random;
   private int thinkCount;
-  private int eatCount;
+  private int thinkTime;
+  private int eatTime;
+  private int cycles;
   private int id;
+  private int hand;
+  private FileWriter newFile;
 
-  public Philosopher(Chopstick left, Chopstick right) {
-    if (left.getId() > right.getId()) {
-      this.first = right; this.second = left;
-    }else {
-      this.first = left; this.second = right;
-    }
-
+  public Philosopher(Chopstick right, Chopstick left, int thinkTime, int eatTime, int cycles, int id, int hand, FileWriter newFile) {
+    this.right = right; this.left = left;
+    this.thinkTime = thinkTime;
+    this.eatTime = eatTime;
+    this.cycles = cycles;
+    this.id = id;
+    this.hand = hand;
+    this.newFile = newFile;
     random = new Random();
+
+    if (cycles == 0){
+      thinkCount = Integer.MIN_VALUE;
+    }
   }
 
   public void run() {
     try {
-      while(true) {
-        ++thinkCount;
-        ++eatCount;
-        ++id;
-        //if (thinkCount % 10 == 0)
-        System.out.println("Philosopher " + id  + " thinks " + thinkCount + " units");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(first) {
-          // Grab right chopstick
-          System.out.println("Philosopher " + id  + " wants right chopstick");
-          System.out.println("Philosopher " + id  + " has right chopstick");
-          synchronized(second) {
-            // Grab left chopstick
-            System.out.println("Philosopher " + id  + " wants left chopstick");
-            System.out.println("Philosopher " + id + " has left chopstick");
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+      // Right Handed Philosophers are designated as 0.
+      if (hand == 0){
+        while (thinkCount < cycles) {
+          ++thinkCount;
+          if (thinkCount % 10 == 0)
+            newFile.write("Philosopher " + id + " has thought " + thinkCount + " times" + "\n");
+          thinkTime = 1000;
+          int tt = random.nextInt(thinkTime);
+          newFile.write("Philosopher " + id + " thinks for " + tt + " units" + "\n");
+          Thread.sleep(tt);     // Think for a while
+          newFile.write("Philosopher " + id + " wants right chopstick"+ "\n");
+          synchronized(right) {                    // Grab right chopstick
+            newFile.write("Philosopher " + id + " has right chopstick"+ "\n");
+            newFile.write("Philosopher " + id + " wants left chopstick"+ "\n");
+            synchronized(left) {
+              newFile.write("Philosopher " + id + " has left chopstick" + "\n");
+              // Grab left chopstick
+              eatTime = 1000;
+              int et = random.nextInt((eatTime));
+              newFile.write("Philosopher " + id + " eats for " + et + " units" + "\n");
+              Thread.sleep(et); // Eat for a while
+            }
+            newFile.write("Philosopher " + id + " releases left chopstick" + "\n");
           }
-
-          System.out.println("Philosopher " + id + " eats for " + eatCount + " units");
-          Thread.sleep(random.nextInt(1000));
-          System.out.println("Philosopher " + id + " releases left chopstick");
-          System.out.println("Philosopher " + id  + " releases right chopstick");
+          newFile.write("Philosopher " + id + " releases right chopstick" + "\n");
         }
       }
-    } catch(InterruptedException e) {}
+
+      // Start with even numbers of philosophers that are right-handed.
+      else{
+        // Right Handed Philosopher with even numbers.
+        if (id % 2 == 0){
+          while (thinkCount < cycles) {
+            ++thinkCount;
+            if (thinkCount % 10 == 0)
+              newFile.write("Philosopher " + id + " has thought " + thinkCount + " times" + "\n");
+            thinkTime = 1000;
+            int tt = random.nextInt(thinkTime);
+            newFile.write("Philosopher " + id + " thinks for " + tt + " units" + "\n");
+            Thread.sleep(tt);     // Think for a while
+            newFile.write("Philosopher " + id + " wants right chopstick"+ "\n");
+            synchronized(right) {                    // Grab right chopstick
+              newFile.write("Philosopher " + id + " has right chopstick"+ "\n");
+              newFile.write("Philosopher " + id + " wants left chopstick"+ "\n");
+              synchronized(left) {
+                newFile.write("Philosopher " + id + " has left chopstick");
+                eatTime = 1000;
+                int et = random.nextInt((eatTime));
+
+                newFile.write("Philosopher " + id + " eats for " + et + " units" + "\n");
+                Thread.sleep(et); // Eat for a while
+              }
+              newFile.write("Philosopher " + id + " releases left chopstick" + "\n");
+            }
+            newFile.write("Philosopher " + id + " releases right chopstick" + "\n");
+
+          }
+        }
+        // Left Handed Philosopher that are even.
+        else if (id % 2 != 0){
+          while (thinkCount < cycles) {
+            ++thinkCount;
+            if (thinkCount % 10 == 0)
+              newFile.write("Philosopher " + id + " has thought " + thinkCount + " times" + "\n");
+            thinkTime =  1000;
+            int tt = random.nextInt(thinkTime);
+            newFile.write("Philosopher " + id + " thinks for " + tt + " units" + "\n");
+            Thread.sleep(tt);     // Think for a while
+
+            newFile.write("Philosopher " + id + " wants left chopstick"+ "\n");
+            synchronized(left) {                    // Grab left chopstick
+              newFile.write("Philosopher " + id + " has left chopstick"+ "\n");
+              newFile.write("Philosopher " + id + " wants right chopstick"+ "\n");
+              synchronized(right) {
+                newFile.write("Philosopher " + id + " has right chopstick"+ "\n");
+                // Grab right chopstick
+                eatTime = 1000;
+                int et = random.nextInt((eatTime));
+
+                System.out.println("Philosopher " + id + " eats for " + et + " units" + "\n");
+                Thread.sleep(et); // Eat for a while
+              }
+              newFile.write("Philosopher " + id + " releases right chopstick" + "\n");
+            }
+            newFile.write("Philosopher " + id + " releases left chopstick" + "\n");
+          }
+
+        }
+      }
+    } catch(InterruptedException e) {} catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
